34eb66357761dd234781121a3008dba06335351d
marcos-vini-uag <ortigol2010@gmail.com>
2019-11-12 03:22:43 -0200

    testes de model para usuario e advogado

--- test/models/advogado_test.rb
+++ test/models/advogado_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class AdvogadoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create advogado with nome, email and n_OAB' do
+    advogado = Advogado.new nome: 'Marcos', email: 'marcos@gmail.com', n_OAB: '123456'
+    assert advogado.save
+  end
+
+  test 'should update advogado with nome, and n_OAB' do
+    advogado = Advogado.new nome: 'Marcos Vinicius', n_OAB: '122345'
+    assert advogado.save
+  end
+
+  test 'should not create advogado without nome, email and n_OAB' do
+    advogado = Advogado.new
+    assert_not advogado.save
+  end
 end
