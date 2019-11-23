d6091003d7325a2879f83e961bb281c1bfd733c9
Yuri Resende <yresendeo@gmail.com>
2019-11-23 19:07:58 -0300

    Relacionamento advogado-user e alteração das views

--- db/schema.rb
+++ db/schema.rb
@@ -48,13 +48,11 @@ ActiveRecord::Schema.define(version: 20191111174622) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "advogado_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["advogado_id"], name: "index_users_on_advogado_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
