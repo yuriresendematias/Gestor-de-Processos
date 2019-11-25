1ac9d134906e7040536ced8be54021bb2fac8fa2
Yuri Resende <yresendeo@gmail.com>
2019-11-25 00:45:06 -0300

    adicionando o dependent: :destroy nos models

--- app/models/advogado.rb
+++ app/models/advogado.rb
@@ -1,6 +1,6 @@
 class Advogado < ApplicationRecord
   belongs_to :user
-  has_many :processos
+  has_many :processos, dependent: :destroy
 
   validates :nome, presence: true, length: { minimum: 3 }
   validates :n_OAB, presence: true, length: { is: 6 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :advogado
+  has_one :advogado, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
